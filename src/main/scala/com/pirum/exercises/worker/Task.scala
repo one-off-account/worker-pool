72d56ad26ce8d92aff9e24a7f4145fb575a21b5d
one-off-account <85136193+one-off-account@users.noreply.github.com>
2021-06-01 13:39:24 +0100

    Implement Task model

--- src/main/scala/com/pirum/exercises/worker/Task.scala
+++ src/main/scala/com/pirum/exercises/worker/Task.scala
@@ -1,6 +1,9 @@
 package com.pirum.exercises.worker
 
-// A task that either succeeds after n seconds, fails after n seconds, or never terminates
-sealed trait Task {
-  def execute: Future[Unit]
-}
+import scala.concurrent.duration.FiniteDuration
+
+sealed trait Action
+case object Throws extends Action
+case object Completed extends Action
+
+case class Task(action: Action, duration: FiniteDuration)
