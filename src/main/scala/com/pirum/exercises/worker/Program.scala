75de4581c168cffc9db81641725b591f7d70cd6e
one-off-account <85136193+one-off-account@users.noreply.github.com>
2021-06-01 13:48:52 +0100

    Create Actors
    - Setup task list
    - Setup number of workers
    - Setup time out

--- src/main/scala/com/pirum/exercises/worker/Program.scala
+++ src/main/scala/com/pirum/exercises/worker/Program.scala
@@ -1,7 +1,30 @@
 package com.pirum.exercises.worker
 
-import scala.concurrent.duration.FiniteDuration
+import akka.actor.Props
+import akka.routing.RoundRobinPool
+import com.pirum.exercises.worker.WorkerPool.system
+import scala.concurrent.duration._
+import scala.collection.mutable.ListBuffer
+import scala.language.postfixOps
 
-trait Program {
-  def program(tasks: List[Task], timeout: FiniteDuration, workers: Int): Unit
+object Program {
+
+  var successful = ListBuffer[Task]()
+  var failure = ListBuffer[Task]()
+  var timedOut = ListBuffer[Task]()
+
+  val taskList = List(
+    Task(Throws, 4 seconds),
+    Task(Completed, 3 seconds),
+    Task(Throws, 50 seconds),
+    Task(Completed, 5 seconds),
+    Task(Throws, 1 seconds)
+  )
+
+  val numberOfWorkers = 4
+  val timeout = 8 seconds
+
+  val master = system.actorOf(Props(new Master(taskList, timeout)), "master")
+  val router = system.actorOf(RoundRobinPool(numberOfWorkers).props(Props[Worker]()), "router")
+  val worker = system.actorOf(Props[Worker], "worker")
 }
